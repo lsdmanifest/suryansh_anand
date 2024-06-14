7782eff98ee77f273ef500ca3726bdab9cf1f752
lsdmanifest <lsd.manifest@gmail.com>
2024-06-14 13:26:20 +0300

    delete NextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 import { fileURLToPath } from "node:url";
 import createJiti from "jiti";
-import {NextConfig} from "next";
 const jiti = createJiti(fileURLToPath(import.meta.url));
 
 // Import env here to validate during build. Using jiti we can import .ts files :)
