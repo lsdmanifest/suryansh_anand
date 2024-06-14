446960bb209e76bed2452540a1a8fa93f8c8977f
lsdmanifest <lsd.manifest@gmail.com>
2024-06-14 13:07:02 +0300

    add .env env.ts and t3env with neon and drizzle orm

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
+import { fileURLToPath } from "node:url";
+import createJiti from "jiti";
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+// Import env here to validate during build. Using jiti we can import .ts files :)
+jiti("./lib/env");
 const nextConfig = {};
 
 export default nextConfig;
